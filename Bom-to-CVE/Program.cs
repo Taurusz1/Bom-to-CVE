a40d89ddd2150d7a2b8357c3c4f4c4d40112990c
Taurusz1 <100237601+Taurusz1@users.noreply.github.com>
2022-11-23 10:41:14 +0100

    bom read and parsed

--- Bom-to-CVE/Program.cs
+++ Bom-to-CVE/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Xml;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+class ReadXML
+{
+    static void Main(string[] args)
+    {
+        XmlDocument doc = new XmlDocument();
+        string path = "C:\\Users\\Bazsó\\Desktop\\5.félév\\Témalab\\boms\\PetriNet.xml";
+
+        doc.Load(args[0]);
+        XmlNodeList list = doc.GetElementsByTagName("name");
+        for (int i = 0; i < list.Count; i++)
+        {
+            Console.WriteLine(list[i].InnerText.ToString());
+        }
+    }
+   
+}
